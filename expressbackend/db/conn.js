61e51d3b24bd467592d2ebd8578df868af16b971
Shubham Nagane <36560341+admiral44@users.noreply.github.com>
2022-03-17 16:11:13 +0530

    Update conn.js

--- expressbackend/db/conn.js
+++ expressbackend/db/conn.js
@@ -1,1 +1,14 @@
+import mongoose from "mongoose";
 
+const connDB = async (DATABASE_URL) => {
+    try {
+        const DB_OPATION = {
+            dbName : 'Org',
+        }
+        await mongoose.connect(DATABASE_URL, DB_OPATION);
+        console.log("connection successful...");
+    } catch (error) {
+        console.log("Error : "+ error);
+    }
+}
+export default connDB
