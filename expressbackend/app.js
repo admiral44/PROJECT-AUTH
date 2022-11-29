6adbaefcfb6c6c403bb5b9d5ce7e2944409e2859
Shubham Nagane <36560341+admiral44@users.noreply.github.com>
2022-11-29 20:13:04 +0530

    Update app.js

--- expressbackend/app.js
+++ expressbackend/app.js
@@ -5,7 +5,7 @@ import cors from 'cors'
 
 const app = express()
 const port = process.env.PORT || '8000'
-const DATABASE_URL = process.env. DATABASE_URL || 'mongodb://localhost:27017';
+const DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost:27017';
 
 // cors policy error handling.
 app.use(cors())
